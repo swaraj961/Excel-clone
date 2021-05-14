099a2b7b1f700e769d5de97069d395262fad9a89
Swaraj routray <swarajroutray961@gmail.com>
2021-05-14 18:43:35 +0530

    color-picker

--- script.js
+++ script.js
@@ -341,3 +341,36 @@ function setStyle(ele, property, key,value){
         }
     
 }
+
+$(".pick-color").colorPick({
+    'initialColor': "#abcd",
+    'allowRecent': true,
+    'recentMax': 5,
+    'allowCustomColor': true,
+    'palette': ["#1abc9c", "#16a085", "#2ecc71", "#27ae60", "#3498db", "#2980b9", "#9b59b6", "#8e44ad", "#34495e", "#2c3e50", "#f1c40f", "#f39c12", "#e67e22", "#d35400", "#e74c3c", "#c0392b", "#ecf0f1", "#bdc3c7", "#95a5a6", "#7f8c8d"],
+    'onColorSelected': function () {
+        if(this.color != "#ABCD") {
+            if($(this.element.children()[1]).attr("id") == "fill-color") {
+                $(".input-cell.selected").css("background-color",this.color);
+                $("#fill-color").css("border-bottom",`4px solid ${this.color}`);
+            }
+            if($(this.element.children()[1]).attr("id") == "text-color") {
+                $(".input-cell.selected").css("color",this.color);
+                $("#text-color").css("border-bottom",`4px solid ${this.color}`)
+            }
+        }
+    }
+});
+
+
+$("#fill-color").click(function(e) {
+    setTimeout(() => {
+        $(this).parent().click();
+    }, 10);
+});
+
+$("#text-color").click(function(e) {
+    setTimeout(() => {
+        $(this).parent().click();
+    }, 10);
+});
